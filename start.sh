34a5aafcf64e8d4d6935b67014b3c431976ea4ef
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2023-02-24 19:31:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/hagadmansa.git /hagadmansa
+  git clone https://github.com/Munnipopz/hagadmansa.git /hagadmansa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /hagadmansa
